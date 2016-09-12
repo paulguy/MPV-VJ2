9d9c5ad2a07840a83ad7dd4a328400256ef0ca2d
Paul LaMendola <paulguy119@gmail.com>
2016-09-12 18:30:03 -0400

    Changed license text to actual comments. Fixed SettingsWindow bug.

--- SettingsDialog.py
+++ SettingsDialog.py
@@ -1,21 +1,20 @@
-"""
-MPV-VJ2 Copyright 2016 paulguy <paulguy119@gmail.com>
+# MPV-VJ2 Copyright 2016 paulguy <paulguy119@gmail.com>
+#
+# This file is part of MPV-VJ2.
+#
+# MPV-VJ2 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MPV-VJ2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MPV-VJ2.  If not, see <http://www.gnu.org/licenses/>.
 
-This file is part of MPV-VJ2.
-
-MPV-VJ2 is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-MPV-VJ2 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with MPV-VJ2.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -93,11 +92,9 @@ class SettingsDialog(Gtk.Dialog):
 
   def delKey(self, button):
     row, column = self.listView.get_cursor()
-    #i = self.listStore.get_iter(row)
-    #key = self.listStore.get_value(i, 0)
-    key = self.listStore[0][row.get_indices()[0]]
+    key = self.listStore[row.get_indices()[0]][0]
     del self.options[key]
-    self.listStore.remove(i)
+    del self.listStore[row.get_indices()[0]]
 
   def __init__(self, lastWindow, options, add=False):
     Gtk.Dialog.__init__(self, title="Settings")
